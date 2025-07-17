d7fcc9de8e1675c90389848a5901537db612bdd0
Parth2684 <bhosle6006@gmail.com>
2025-07-17 15:06:16 +0530

    making a commit from the root dir of tyurborepo

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,7 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import { FRONTEND_URL } from './config'
+import sessionMiddleware from './middlewares/session'
 
 
 
@@ -24,7 +25,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
-app.use()
+app.use(sessionMiddleware)
 
 
 
--- packages/database/src/client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_1 = require("../generated/prisma");
-var globalForPrisma = global;
-var prisma = globalForPrisma.prisma || new prisma_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production')
-    globalForPrisma.prisma = prisma;
-exports.default = prisma;
